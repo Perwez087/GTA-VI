a987276b21dc1125d9a54f6187c022c601c75a9c
Perwez087 <parwez4568@gmail.com>
2025-08-14 00:24:33 +0530

    tailwind config

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  theme: {
+    extend: {
+      fontFamily: {
+        pricedown: ['"pricedown"', 'sans-serif'],
+        helvetica: ['"Helvetica Now Display"', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+}
